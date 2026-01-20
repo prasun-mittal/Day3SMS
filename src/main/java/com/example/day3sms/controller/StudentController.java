f71149192edd6807d219fdc942573363216a1f79
Prasun Mittal <prasunmittal09@gmail.com>
2026-01-20 14:08:57 +0530

    Initial commit

---
+++ src/main/java/com/example/day3sms/controller/StudentController.java
@@ -0,0 +1,12 @@
+package com.example.day3sms.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+    @GetMapping("/")
+    public String student(){
+        return "Homepage";
+    }
+}
